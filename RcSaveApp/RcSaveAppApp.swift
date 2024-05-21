56b379099ed1eaaf242206158335639611c9c643
Sako-0715 <sakou.0715@gmail.com>
2024-05-22 00:33:05 +0900

    Initial Commit

---
+++ RcSaveApp/RcSaveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RcSaveAppApp.swift
+//  RcSaveApp
+//
+//  Created by 酒匂竜也 on 2024/05/22.
+//
+
+import SwiftUI
+
+@main
+struct RcSaveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
